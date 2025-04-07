162001675b95f52aaa29d65ad54d4bd4dff31aee
Vlad <v.redula@faradaygroup.com>
2025-04-07 12:13:19 +0900

    Adding leave and using month and year to filter the liast

--- Scheduling/Controllers/ScheduleController.cs
+++ Scheduling/Controllers/ScheduleController.cs
@@ -19,8 +19,6 @@ namespace Scheduling.Controllers
 
         public async Task<IActionResult> Index(int month = 0, int year = 0, int departmentId = 0)
         {
-            var user = await ThisUser();
-
             if (month == 0)
                 month = DateTime.Now.Month;
 
@@ -29,51 +27,42 @@ namespace Scheduling.Controllers
 
             if (departmentId == 0)
             {
+                var user = await ThisUser();
                 departmentId = user.Department_ID.Value;
             }
 
-            var users = _db.Users.Where(u => (u.Privilege_ID == 1 || u.Privilege_ID == 2) && u.Department_ID == departmentId).ToList();
             var shifts = _db.Shifts.ToList();
-            var schedules = _db.Schedules.Include(s => s.User).Include(s => s.Shift).Include(s => s.Leave).ToList();
             var holidays = _db.Holidays.ToList();
 
-            ViewBag.Departments = new SelectList(_db.Departments.ToList(), "Department_ID", "Department_name", departmentId);
-            ViewBag.Leaves = _db.Leaves.ToList();
-
-            return View((users, shifts, schedules, holidays, month, year, departmentId));
-        }
-
-        [Authorize(Roles = "admin,manager")]
-        public async Task<IActionResult> Manage(int month = 0, int year = 0, int departmentId = 0)
-        {
-            var user = await ThisUser();
-
-            if (month == 0)
-                month = DateTime.Now.Month;
-
-            if (year == 0)
-                year = DateTime.Now.Year;
-
-            if (departmentId == 0)
-            {
-                departmentId = user.Department_ID.Value;
-            }
-
             var users = _db.Users
-                .Where(u => (u.Privilege_ID == 1 || u.Privilege_ID == 2) && u.Department_ID == departmentId)
+                .Where(u => (u.Privilege_ID != 0 && u.Privilege_ID != 4) && u.Department_ID == departmentId && u.Status == 1)
                 .OrderBy(u => u.Sector_ID)
                 .ThenBy(u => u.Privilege_ID)
                 .ThenBy(u => u.First_name)
                 .ThenBy(u => u.Last_name)
                 .ToList();
-            var shifts = _db.Shifts.ToList();
-            var schedules = _db.Schedules.Include(s => s.User).Include(s => s.Shift).Include(s => s.Leave).ToList();
-            var holidays = _db.Holidays.ToList();
+
+            var schedules = _db.Schedules
+                .Include(s => s.User)
+                .Include(s => s.Shift)
+                .Where(s => s.Date.Month == month && s.Date.Year == year)
+                .ToList();
+
+            var leaves = _db.Leaves
+                .Where(l => 
+                    (l.Date_start.Year == year && l.Date_start.Month == month) || 
+                    (l.Date_end.Year == year && l.Date_end.Month == month) &&
+                    l.Status != "Cancelled" && l.Status != "Denied")
+                .ToList();
 
             ViewBag.Departments = new SelectList(_db.Departments.ToList(), "Department_ID", "Department_name", departmentId);
-            ViewBag.Leaves = _db.Leaves.ToList();
+            ViewBag.LeaveTypes = _db.Leave_types.ToList();
 
-            return View((users, shifts, schedules, holidays, month, year, departmentId));
+
+            if (User.IsInRole("member"))
+                return View((users, shifts, schedules, leaves, holidays, month, year));
+            else
+                return View("Manage", (users, shifts, schedules, leaves, holidays, month, year));
         }
 
         [HttpPost]
@@ -91,7 +80,6 @@ namespace Scheduling.Controllers
                 } else
                 {
                     // Update existing schedule with new shift
-                    existingSchedule.Leave_ID = null;
                     existingSchedule.Shift_ID = shiftId;
                     _db.Schedules.Update(existingSchedule);
                 }
@@ -115,76 +103,6 @@ namespace Scheduling.Controllers
 
         [HttpPost]
         [Authorize(Roles = "admin,manager")]
-        public async Task<IActionResult> AssignLeave(int userId, int leaveId, DateTime date)
-        {
-            var existingLeave = await _db.Schedules
-                .FirstOrDefaultAsync(s => s.Personnel_ID == userId && s.Date == date);
-
-            if (existingLeave != null)
-            {
-                // Update existing schedule with new leave
-                existingLeave.Shift_ID = null;
-                existingLeave.Leave_ID = leaveId;
-                _db.Schedules.Update(existingLeave);
-            }
-            else
-            {
-                // Create a new schedule
-                var newSchedule = new Schedule
-                {
-                    Personnel_ID = userId,
-                    Leave_ID = leaveId,
-                    Date = date
-                };
-                _db.Schedules.Add(newSchedule);
-            }
-
-            await _db.SaveChangesAsync();
-
-            return Json(new { success = true });
-        }
-
-        [Authorize(Roles = "admin,manager")]
-        public async Task<IActionResult> AssignLeaves(int userId, int leaveId, DateTime dateStart, DateTime dateEnd, string comment = "")
-        {
-            for (DateTime date = dateStart; date <= dateEnd; date = date.AddDays(1))
-            {
-                var existingLeave = await _db.Schedules
-                    .FirstOrDefaultAsync(s => s.Personnel_ID == userId && s.Date == date);
-
-                if (existingLeave != null)
-                {
-                    // Update existing schedule with new leave
-                    existingLeave.Shift_ID = null;
-                    existingLeave.Leave_ID = leaveId;
-                    existingLeave.Comment = comment;
-                    _db.Schedules.Update(existingLeave);
-
-                    await _db.SaveChangesAsync();
-                }
-                else
-                {
-                    // Create a new schedule
-                    var newSchedule = new Schedule
-                    {
-                        Personnel_ID = userId,
-                        Leave_ID = leaveId,
-                        Date = date,
-                        Comment = comment
-                    };
-
-                    _db.Schedules.Add(newSchedule);
-                    await _db.SaveChangesAsync();
-                }
-            }
-
-            var user = await ThisUser();
-
-            return RedirectToAction("Manage", (dateStart.Month, dateStart.Year, user.Department_ID));
-        }
-
-        [HttpPost]
-        [Authorize(Roles = "admin,manager")]
         public async Task<IActionResult> RemoveShift(int scheduleId)
         {
             var schedule = await _db.Schedules.FindAsync(scheduleId);
@@ -200,18 +118,24 @@ namespace Scheduling.Controllers
         [Authorize]
         public IActionResult GetScheduleByMonth(int month, int year, int departmentId)
         {
-            var users = _db.Users.Where(u => (u.Privilege_ID == 1 || u.Privilege_ID == 2) && u.Department_ID == departmentId).ToList();
+            var users = _db.Users.Where(u => (u.Privilege_ID != 0 && u.Privilege_ID != 4) && u.Department_ID == departmentId && u.Status == 1).ToList();
             var shifts = _db.Shifts.ToList();
             var schedules = _db.Schedules
                 .Include(s => s.Shift)
-                .Include(s => s.Leave)
                 .Where(s => s.Date.Year == year && s.Date.Month == month)
                 .ToList();
 
-            ViewBag.Leaves = _db.Leaves.ToList();
             var holidays = _db.Holidays.ToList();
+            var leaves = _db.Leaves
+                .Include(l => l.Leave_type)
+                .Where(l =>
+                    (l.Date_start.Year == year && l.Date_start.Month == month) ||
+                    (l.Date_end.Year == year && l.Date_end.Month == month))
+                .ToList();
 
-            return PartialView("_ScheduleTable", (users, shifts, schedules, holidays, month, year, departmentId));
+            ViewBag.LeaveTypes = _db.Leave_types.ToList();
+
+            return PartialView("_ScheduleTable", (users, shifts, schedules, leaves, holidays, month, year));
         }
 
         public int GetPersonnelID()
