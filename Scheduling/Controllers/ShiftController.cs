b74b72e8802b609e58eb398c181bfee1141bf9e8
Vlad <v.redula@faradaygroup.com>
2025-04-10 09:34:18 +0900

    Added shift CRUD

---
+++ Scheduling/Controllers/ShiftController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Scheduling.Models;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Scheduling.Controllers
+{
+    public class ShiftController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ShiftController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (!User.IsInRole("admin"))
+            {
+                var user = await ThisUser();
+                ViewBag.Shifts = _db.Shifts.Where(s => s.Department_ID == user.Department_ID).ToList();
+            } else
+            {
+                ViewBag.Shifts = _db.Shifts.ToList();
+            }
+
+            return View();
+        }
+
+        // POST: Shift/Add
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add([Bind("Shift_name,Time_start,Time_end,Pattern")] Shift shift)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await ThisUser();
+                shift.Department_ID = user.Department_ID;
+
+                await _db.Shifts.AddAsync(shift);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Shifts = _db.Shifts.ToList();
+
+            return View(nameof(Index), shift);
+        }
+
+        // POST: Shift/Edit/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Shift_ID,Shift_name,Time_start,Time_end,Pattern,Department_ID")] Shift shift)
+        {
+            if (id != shift.Shift_ID)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Shifts.Update(shift);
+                    await _db.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Unable to update Shift: {id} - {ex}");
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Shifts = _db.Shifts.ToList();
+
+            return View(nameof(Index), shift);
+        }
+
+
+        // POST: Shift/Delete/id
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shift = await _db.Shifts.FindAsync(id);
+            if (shift == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
+            {
+                _db.Shifts.Remove(shift);
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to delete Shift: {id} - {ex}");
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetShift(int? id)
+        {
+            if (id == null || _db.Shifts == null)
+            {
+                return NoContent();
+            }
+
+            var shift = await _db.Shifts.FindAsync(id);
+            if (shift == null)
+            {
+                return NotFound(new { message = "Shift not found!" });
+            }
+
+            return Ok(shift);
+        }
+
+        public int GetPersonnelID()
+        {
+            var personnelId = int.Parse(User.FindFirstValue("Personnelid"));
+
+            return personnelId;
+        }
+
+        public async Task<string> GetUsername()
+        {
+            var user = await ThisUser();
+            return user.Username;
+        }
+
+        public async Task<User> ThisUser()
+        {
+            return _db.Users.Find(GetPersonnelID());
+        }
+    }
+}
--- Scheduling/Models/Shift.cs
+++ Scheduling/Models/Shift.cs
@@ -1,13 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Scheduling.Models
 {
     public class Shift
     {
         [Key]
+        [Display(Name = "Shift ID")]
         public int Shift_ID { get; set; }
-        public string Shift_name { get; set; } // Shift A, B, C
-        public TimeSpan Time_start { get; set; }
-        public TimeSpan Time_end { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR")]
+        [Display(Name = "Shift name")]
+        [StringLength(25)]
+        public string? Shift_name { get; set; }
+        [Display(Name = "Start time")]
+        public TimeSpan? Time_start { get; set; }
+        [Display(Name = "End time")]
+        public TimeSpan? Time_end { get; set; }
+        public string? Pattern { get; set; }
+        public int? Department_ID { get; set; }
     }
 }
