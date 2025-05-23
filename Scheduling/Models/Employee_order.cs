78508841f6c77bdac92b8852fa58c151d5cef8f1
Vlad <v.redula@faradaygroup.com>
2025-05-23 18:07:08 +0900

    added department_ID and privilege_ID

--- Scheduling/Models/Employee_order.cs
+++ Scheduling/Models/Employee_order.cs
@@ -14,6 +14,8 @@ namespace Scheduling.Models
 
         public int? Order_index { get; set; } // Position in the calendar
         public int? Sector_ID { get; set; } // Team ID
+        public int? Department_ID { get; set; }
+        public int? Privilege_ID { get; set; }
         public int? Year { get; set; }
         public int? Month { get; set; }
     }
